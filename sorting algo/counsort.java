3dcb3a1516b179404e261cb35f74e9208e12c4b2
Omkar Singh <omkar0301ss@gmail.com>
2026-02-03 02:30:47 +0530

    sorting algos
    this folder contains 5 files in which 4 of them is all 4 basic sorting algorithms of java and one is of inbuilt algorithms for sorting

---
+++ sorting algo/counsort.java
@@ -0,0 +1,35 @@
+public class counsort {
+    public static void countingSort(int arr[]){
+        int largest = Integer.MIN_VALUE;
+        for (int i =0; i<arr.length; i++){
+            largest = Math.max(largest,arr[i]);
+
+        }
+        int count[] = new int[largest+1];
+        for (int i=0; i<arr.length; i++){
+            count [arr[i]]++;
+        }
+
+        //sorting
+        int j =0;
+        for(int i=0; i<count.length; i++){
+            while(count[i] > 0){
+                arr[j] =i;
+                j++;
+                count [i]--;
+            }
+        }
+    }
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        int arr[] = {5,3,4,2,3,4,4,5,6,7,1,4,5,7,};
+        countingSort(arr);
+        printArr(arr);
+    }
+    
+}
